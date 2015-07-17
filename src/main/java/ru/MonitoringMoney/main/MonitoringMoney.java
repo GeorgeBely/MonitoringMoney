e8dae7d2a5d4bce95befe6a39119da32ea835a95
gbeliy <man240893@mail.ru>
2015-07-17 16:21:08 +0300

    При добавление "типа покупки",  "уровня важности" или "пользователя" он будет добавлен сразу с список

--- src/main/java/ru/MonitoringMoney/main/MonitoringMoney.java
+++ src/main/java/ru/MonitoringMoney/main/MonitoringMoney.java
@@ -10,7 +10,7 @@ import java.io.*;
 
 public class MonitoringMoney implements Serializable {
 
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
 
 
     /** Основной фрейм приложения */
